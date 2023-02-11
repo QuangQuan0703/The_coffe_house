6401906a23d8e9538acb7b90aa84c515a22b004e
quan <nguyenquangquan_t65@hus.edu.vn>
2023-02-11 20:09:33 +0700

    the coffe house lan2

---
+++ android/app/src/main/java/com/example/huhu/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.huhu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.framelayout);
+    }
+}
